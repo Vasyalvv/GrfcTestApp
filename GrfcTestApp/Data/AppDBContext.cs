abaf6c9b5204efd10db49515278d5f7ae19980df
Vasyalvv <vasyalvv@gmail.com>
2022-09-26 20:54:31 +0500

    Создан контекст БД

---
+++ GrfcTestApp/Data/AppDBContext.cs
@@ -0,0 +1,25 @@
+﻿using GrfcTestApp.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrfcTestApp.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public DbSet<CarMark> CarMarks { get; set; }
+
+        public DbSet<EngineType> EngineTypes { get; set; }
+
+        public DbSet<CarModel> CarModels { get; set; }
+
+        public DbSet<Automobile> Automobiles { get; set; }
+
+        public DbSet<Operation> Operations { get; set; }
+
+        public DbSet<Maintenance> Maintenances { get; set; }
+    }
+}
